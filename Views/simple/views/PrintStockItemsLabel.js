cd80090dbb2faf24110e36b1feb820daaeb00010
Maksym Sliushchynskyi <m.sliushchynskyi@brainence.com>
2021-09-28 15:52:21 +0300

    Fixed styles 2.0

--- Views/simple/views/PrintStockItemsLabel.js
+++ Views/simple/views/PrintStockItemsLabel.js
@@ -1,6 +1,9 @@
 var PrintStockItemsLabelView = function ($scope, $element, $filter, $compile, $q, $http) {
   var self = this;
-  let plkrFrame = document.getElementById("plkrFrame");
+  let plkrFrame = document.getElementById("stockItemsLabelFrame");
   let url_string = $scope._moduleInstance.options.data.URL;
   plkrFrame.src = url_string;
+
+  let iframeContainerParent = document.getElementById("iframe-container").parentNode;
+  iframeContainerParent.style.padding = '0px';
 };
