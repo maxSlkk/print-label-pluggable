2660af828f00ef76e66f11a8e501b98b4771972b
Maksym Sliushchynskyi <m.sliushchynskyi@brainence.com>
2021-09-28 13:53:19 +0300

    asfa

--- Views/simple/views/PrintStockItemsLabel.js
+++ Views/simple/views/PrintStockItemsLabel.js
@@ -1,14 +1,14 @@
 var PrintStockItemsLabelView = function ($scope, $element, $filter, $compile, $q, $http) {
   var self = this;
   alert("hello");
-  $scope.purchaseId = $scope.$parent.$parent.$parent.purchaseOrder.pkPurchaseID;
-  $scope.purchaseNumber = $scope.$parent.$parent.$parent.purchaseOrder.ExternalInvoiceNumber;
-  $scope.purchaseAmount = $scope.$parent.$parent.$parent.purchaseOrder.ConvertedGrandTotal;
-  $scope.userId = $scope.$parent.$parent.$parent.$parent.$parent.$root.session.userId;
+  // $scope.purchaseId = $scope.$parent.$parent.$parent.purchaseOrder.pkPurchaseID;
+  // $scope.purchaseNumber = $scope.$parent.$parent.$parent.purchaseOrder.ExternalInvoiceNumber;
+  // $scope.purchaseAmount = $scope.$parent.$parent.$parent.purchaseOrder.ConvertedGrandTotal;
+  // $scope.userId = $scope.$parent.$parent.$parent.$parent.$parent.$root.session.userId;
   let plkrFrame = document.getElementById("plkrFrame");
   let url_string = "https://pwp-test.herokuapp.com/pluggable/login";
 
-  let frameUrl = url_string + "?userId=" + $scope.userId + "&purchaseNumber=" + $scope.purchaseNumber+ "&purchaseAmount=" + $scope.purchaseAmount;
+  let frameUrl = url_string;
   // let frameUrl = url_string;
 
   plkrFrame.src = frameUrl;
