0b26a8ae2d719856a60436e9b209819f8df8f0ee
Maksym Sliushchynskyi <m.sliushchynskyi@brainence.com>
2021-09-28 15:03:04 +0300

    Added url to window.

--- Views/simple/views/PrintStockItemsLabel.js
+++ Views/simple/views/PrintStockItemsLabel.js
@@ -1,24 +1,6 @@
 var PrintStockItemsLabelView = function ($scope, $element, $filter, $compile, $q, $http) {
   var self = this;
-  alert("hello");
-  // $scope.purchaseId = $scope.$parent.$parent.$parent.purchaseOrder.pkPurchaseID;
-  // $scope.purchaseNumber = $scope.$parent.$parent.$parent.purchaseOrder.ExternalInvoiceNumber;
-  // $scope.purchaseAmount = $scope.$parent.$parent.$parent.purchaseOrder.ConvertedGrandTotal;
-  // $scope.userId = $scope.$parent.$parent.$parent.$parent.$parent.$root.session.userId;
   let plkrFrame = document.getElementById("plkrFrame");
-  let url_string = "https://pwp-test.herokuapp.com/pluggable/login";
-
-  let frameUrl = url_string;
-  // let frameUrl = url_string;
-
-  plkrFrame.src = frameUrl;
-
-  // self.onMessage = function (msg) {
-  //   switch (msg.key) {
-  //     case Core.Messenger.MESSAGE_TYPES.INITIALIZE:
-  //       $scope.Initialize();
-  //   }
-  // };
-
-  
+  let url_string = $scope._moduleInstance.options.data.URL;
+  plkrFrame.src = url_string;
 };
