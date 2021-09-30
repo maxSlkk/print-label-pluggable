ef9ad30f264159ae13781d8e1229ff80dcf05143
Maksym Sliushchynskyi <m.sliushchynskyi@brainence.com>
2021-09-30 15:54:58 +0300

    Made dynamic creating of iframes.

--- Views/simple/views/PrintStockItemsLabel.js
+++ Views/simple/views/PrintStockItemsLabel.js
@@ -1,7 +1,15 @@
 let iframeNumber = 1;
 
 var PrintStockItemsLabelView = function ($scope) {
-  let plkrFrame = document.getElementById("stockItemsLabelFrame");
-  let url_string = $scope._moduleInstance.options.data.URL;
-  plkrFrame.src = url_string;
+  let urlString = $scope._moduleInstance.options.data.URL;
+
+  const newIframe = document.createElement('iframe');
+  newIframe.setAttribute('id', 'stockItemLabelFrame' + iframeNumber);
+  newIframe.setAttribute('src', urlString);
+  newIframe.setAttribute('class', 'stockItemLabelFrame');
+  
+  iframeNumber++;
+  
+  const iframeParent = document.getElementsByClassName('iframe-container')[0].getElementsByClassName('content')[0].getElementsByTagName('div')[0];
+  iframeParent.appendChild(newIframe);
 };
