059bcb6716c12e8c1f3860f730b86d7fb208ac0f
Maksym Sliushchynskyi <m.sliushchynskyi@brainence.com>
2021-09-30 15:39:09 +0300

    Removed unused code.

--- Views/simple/views/PrintStockItemsLabel.js
+++ Views/simple/views/PrintStockItemsLabel.js
@@ -1,5 +1,6 @@
-var PrintStockItemsLabelView = function ($scope, $element, $filter, $compile, $q, $http) {
-  var self = this;
+let iframeNumber = 1;
+
+var PrintStockItemsLabelView = function ($scope) {
   let plkrFrame = document.getElementById("stockItemsLabelFrame");
   let url_string = $scope._moduleInstance.options.data.URL;
   plkrFrame.src = url_string;
