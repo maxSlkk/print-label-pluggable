0c6552b9aa21b0c4760e3dca9e13e9c9ef3062c3
Maksym Sliushchynskyi <m.sliushchynskyi@brainence.com>
2022-03-17 12:05:10 +0200

    Commented code

--- Views/js/placeholder.js
+++ Views/js/placeholder.js
@@ -115,233 +115,233 @@
 
 //PLACEHOLDER FOR ADVANCED PERMISSIONS APP
 
-"use strict";
+// "use strict";
 
-define(function (require) {
-  $(document).ready(function ($scope) {
-    const config = { childList: true, subtree: true };
+// define(function (require) {
+//   $(document).ready(function ($scope) {
+//     const config = { childList: true, subtree: true };
 
-    var select_returnForm;
-    var input_returnForm;
-    var select_resendForm;
-    var input_resendForm;
+//     var select_returnForm;
+//     var input_returnForm;
+//     var select_resendForm;
+//     var input_resendForm;
 
-    var allowedQuantity = 0;
-    var isAllowedQuantitySet = false;
-    var refundSum = 0.0;
-    var isRefundSumSet = false;
+//     var allowedQuantity = 0;
+//     var isAllowedQuantitySet = false;
+//     var refundSum = 0.0;
+//     var isRefundSumSet = false;
 
-    var callback = function (mutationsList, observer) {
-        var returnForm = document.getElementsByName("submissionForm.Return")[0];
-        if (returnForm) {
-            var selects = returnForm.getElementsByTagName("select");
-            if (selects) {
-                for (var select of selects) {
-                    //making return location select readonly
-                    if (select.getAttribute("lw-tst") === "select_returnLocation") {
-                        select.disabled = true;
-                    }
-                    //checking if there is anything selected in category select
-                    if (select.getAttribute("lw-tst") === "select_reasonCategory") {
-                        select_returnForm = select;
-                        select.addEventListener("change", isReturnFormValid);
-                    }
-                }
-            }
+//     var callback = function (mutationsList, observer) {
+//         var returnForm = document.getElementsByName("submissionForm.Return")[0];
+//         if (returnForm) {
+//             var selects = returnForm.getElementsByTagName("select");
+//             if (selects) {
+//                 for (var select of selects) {
+//                     //making return location select readonly
+//                     if (select.getAttribute("lw-tst") === "select_returnLocation") {
+//                         select.disabled = true;
+//                     }
+//                     //checking if there is anything selected in category select
+//                     if (select.getAttribute("lw-tst") === "select_reasonCategory") {
+//                         select_returnForm = select;
+//                         select.addEventListener("change", isReturnFormValid);
+//                     }
+//                 }
+//             }
 
-            //making refund input readonly
-            var inputs = returnForm.getElementsByTagName("input");
-            if (inputs) {
-                for (var input of inputs) {
-                    if (input.getAttribute("lw-tst") === "input_Refund") {
-                        if (!isRefundSumSet) {
-                            refundSum = parseFloat(input.value);
-                            isRefundSumSet = true;
-                        }
-                        input.setAttribute('readonly', true);
-                    }
+//             //making refund input readonly
+//             var inputs = returnForm.getElementsByTagName("input");
+//             if (inputs) {
+//                 for (var input of inputs) {
+//                     if (input.getAttribute("lw-tst") === "input_Refund") {
+//                         if (!isRefundSumSet) {
+//                             refundSum = parseFloat(input.value);
+//                             isRefundSumSet = true;
+//                         }
+//                         input.setAttribute('readonly', true);
+//                     }
 
-                    if (input.getAttribute("lw-tst") === "input_returnQuantity") {
-                        if (!isAllowedQuantitySet) {
-                            allowedQuantity = parseInt(input.getAttribute("max"));
-                            input.setAttribute("min", allowedQuantity);
-                            isAllowedQuantitySet = true;
-                        }
-                        input_returnForm = input;
-                        input.addEventListener("change", isReturnFormValid);
-                    }
-                }
-            }
+//                     if (input.getAttribute("lw-tst") === "input_returnQuantity") {
+//                         if (!isAllowedQuantitySet) {
+//                             allowedQuantity = parseInt(input.getAttribute("max"));
+//                             input.setAttribute("min", allowedQuantity);
+//                             isAllowedQuantitySet = true;
+//                         }
+//                         input_returnForm = input;
+//                         input.addEventListener("change", isReturnFormValid);
+//                     }
+//                 }
+//             }
 
-            //checking if button need to be disabled
-            isReturnFormValid();
-        }
-        else {
-            allowedQuantity = 0;
-            isAllowedQuantitySet = false;
-            refundSum = 0.0;
-            isRefundSumSet = false;
-        }
+//             //checking if button need to be disabled
+//             isReturnFormValid();
+//         }
+//         else {
+//             allowedQuantity = 0;
+//             isAllowedQuantitySet = false;
+//             refundSum = 0.0;
+//             isRefundSumSet = false;
+//         }
         
-        //removing exchange tab and form
-        var exchangeForm = document.getElementsByName("submissionForm.Exchange")[0];
-        if (exchangeForm) {
-            var lis = exchangeForm.parentElement.parentElement.parentElement.getElementsByTagName("li");
-            if (lis) {
-                for (var li of lis) {
-                    if (li.getAttribute("lw-tst") === "tab_Exchange") {
-                        //removing tab
-                        li.remove();
-                        //removing tab content
-                        exchangeForm.parentElement.remove();
-                        break;
-                    }
-                }
-            }
-        }
+//         //removing exchange tab and form
+//         var exchangeForm = document.getElementsByName("submissionForm.Exchange")[0];
+//         if (exchangeForm) {
+//             var lis = exchangeForm.parentElement.parentElement.parentElement.getElementsByTagName("li");
+//             if (lis) {
+//                 for (var li of lis) {
+//                     if (li.getAttribute("lw-tst") === "tab_Exchange") {
+//                         //removing tab
+//                         li.remove();
+//                         //removing tab content
+//                         exchangeForm.parentElement.remove();
+//                         break;
+//                     }
+//                 }
+//             }
+//         }
       
-        var resendForm = document.getElementsByName("submissionForm.Resend")[0];
-        if (resendForm) {
-            var selects = resendForm.getElementsByTagName("select");
-            if (selects) {
-                for (var select of selects) {
-                    //making return location select readonly
-                    if (select.getAttribute("lw-tst") === "select_RMAOrderLocation") {
-                        select.disabled = true;
-                    }
-                    //checking if there is anything selected in category select
-                    if (select.getAttribute("lw-tst") === "select_reasonCategory") {
-                        select_resendForm = select;
-                        select.addEventListener("change", isResendFormValid);
-                    }
-                }
-            }
+//         var resendForm = document.getElementsByName("submissionForm.Resend")[0];
+//         if (resendForm) {
+//             var selects = resendForm.getElementsByTagName("select");
+//             if (selects) {
+//                 for (var select of selects) {
+//                     //making return location select readonly
+//                     if (select.getAttribute("lw-tst") === "select_RMAOrderLocation") {
+//                         select.disabled = true;
+//                     }
+//                     //checking if there is anything selected in category select
+//                     if (select.getAttribute("lw-tst") === "select_reasonCategory") {
+//                         select_resendForm = select;
+//                         select.addEventListener("change", isResendFormValid);
+//                     }
+//                 }
+//             }
 
-            //making refund input readonly
-            var inputs = resendForm.getElementsByTagName("input");
-            if (inputs) {
-                for (var input of inputs) {
-                    if (input.getAttribute("lw-tst") === "input_Refund") {
-                        input.value = refundSum;
-                        input.setAttribute('readonly', true);
-                    }
+//             //making refund input readonly
+//             var inputs = resendForm.getElementsByTagName("input");
+//             if (inputs) {
+//                 for (var input of inputs) {
+//                     if (input.getAttribute("lw-tst") === "input_Refund") {
+//                         input.value = refundSum;
+//                         input.setAttribute('readonly', true);
+//                     }
 
-                    if (input.getAttribute("lw-tst") === "input_resendQuantity") {
-                        input.setAttribute("max", allowedQuantity);
-                        input_resendForm = input;
-                        input.addEventListener("change", isResendFormValid);
-                    }
+//                     if (input.getAttribute("lw-tst") === "input_resendQuantity") {
+//                         input.setAttribute("max", allowedQuantity);
+//                         input_resendForm = input;
+//                         input.addEventListener("change", isResendFormValid);
+//                     }
 
-                    if (input.getAttribute("lw-tst") === "input_additionalCost") {
-                        input.setAttribute('readonly', true);
-                    }
-                }
-            }
+//                     if (input.getAttribute("lw-tst") === "input_additionalCost") {
+//                         input.setAttribute('readonly', true);
+//                     }
+//                 }
+//             }
 
-            //checking if button need to be disabled
-            isResendFormValid();
-        }
+//             //checking if button need to be disabled
+//             isResendFormValid();
+//         }
 
-        var refundWindow = document.getElementsByClassName("Refund_AddView")[0];
-        if (refundWindow) {
-            var inputs = refundWindow.getElementsByTagName("input");
-            if (inputs) {
-                for (var input of inputs) {
-                    if (input.getAttribute("lw-tst") === "number") {
-                        var value = input.getAttribute("max");
-                        input.value = parseFloat(value);
-                        input.setAttribute('readonly', true);
-                    }
-                }
-            }
-        }
-    };
+//         var refundWindow = document.getElementsByClassName("Refund_AddView")[0];
+//         if (refundWindow) {
+//             var inputs = refundWindow.getElementsByTagName("input");
+//             if (inputs) {
+//                 for (var input of inputs) {
+//                     if (input.getAttribute("lw-tst") === "number") {
+//                         var value = input.getAttribute("max");
+//                         input.value = parseFloat(value);
+//                         input.setAttribute('readonly', true);
+//                     }
+//                 }
+//             }
+//         }
+//     };
 
-    function isReturnFormValid() {
-        var btn = getSubmitButton("Add Return");
-        if (!btn) {
-            return;
-        }
+//     function isReturnFormValid() {
+//         var btn = getSubmitButton("Add Return");
+//         if (!btn) {
+//             return;
+//         }
 
-        if (!select_returnForm || !input_returnForm) {
-            btn.disabled = true;
-            return;
-        }
+//         if (!select_returnForm || !input_returnForm) {
+//             btn.disabled = true;
+//             return;
+//         }
 
-        if (select_returnForm.value === "?") {
-            btn.disabled = true;
-            return;
-        }
+//         if (select_returnForm.value === "?") {
+//             btn.disabled = true;
+//             return;
+//         }
 
-        if (!isNum(input_returnForm.value) || parseInt(input_returnForm.value) <= 0 
-            || parseInt(input_returnForm.value) != allowedQuantity) {
-            btn.disabled = true;
-            return;
-        }
+//         if (!isNum(input_returnForm.value) || parseInt(input_returnForm.value) <= 0 
+//             || parseInt(input_returnForm.value) != allowedQuantity) {
+//             btn.disabled = true;
+//             return;
+//         }
 
-        btn.disabled = false;
-    }
+//         btn.disabled = false;
+//     }
 
-    function isResendFormValid() {
-        var btn = getSubmitButton("Add Resend");
-        if (!btn) {
-            return;
-        }
+//     function isResendFormValid() {
+//         var btn = getSubmitButton("Add Resend");
+//         if (!btn) {
+//             return;
+//         }
         
-        if (!select_resendForm || !input_resendForm) {
-            btn.disabled = true;
-            return;
-        }
+//         if (!select_resendForm || !input_resendForm) {
+//             btn.disabled = true;
+//             return;
+//         }
 
-        if (select_resendForm.value === "?") {
-            btn.disabled = true;
-            return;
-        }
+//         if (select_resendForm.value === "?") {
+//             btn.disabled = true;
+//             return;
+//         }
 
-        if (!isNum(input_resendForm.value) || parseInt(input_resendForm.value) <= 0 
-            || parseInt(input_resendForm.value) > allowedQuantity) {
-            btn.disabled = true;
-            return;
-        }
+//         if (!isNum(input_resendForm.value) || parseInt(input_resendForm.value) <= 0 
+//             || parseInt(input_resendForm.value) > allowedQuantity) {
+//             btn.disabled = true;
+//             return;
+//         }
 
-        btn.disabled = false;
-    }
+//         btn.disabled = false;
+//     }
 
-    function isNum (str) {
-        return /^\d+$/.test(str);
-    }
+//     function isNum (str) {
+//         return /^\d+$/.test(str);
+//     }
 
-    function getSubmitButton(text) {
-        var btnsDiv = document.getElementsByClassName("buttons")[1];
-        if (btnsDiv) {
-            var buttons = btnsDiv.getElementsByTagName("button");
-            if (buttons) {
-                for (var i = 0; i < buttons.length; i++) {
-                    if (buttons[i].firstChild.nodeValue === text) {
-                        return buttons[i];
-                    }
-                }
-            }
-        }
-    }
+//     function getSubmitButton(text) {
+//         var btnsDiv = document.getElementsByClassName("buttons")[1];
+//         if (btnsDiv) {
+//             var buttons = btnsDiv.getElementsByTagName("button");
+//             if (buttons) {
+//                 for (var i = 0; i < buttons.length; i++) {
+//                     if (buttons[i].firstChild.nodeValue === text) {
+//                         return buttons[i];
+//                     }
+//                 }
+//             }
+//         }
+//     }
 
-    const observer = new MutationObserver(callback);
+//     const observer = new MutationObserver(callback);
 
-    const session = JSON.parse(window.localStorage.getItem("SPA_auth_session"));
+//     const session = JSON.parse(window.localStorage.getItem("SPA_auth_session"));
 
-    const userPermissions = JSON.parse(getUserPermissions(session.userName, session.token));
+//     const userPermissions = JSON.parse(getUserPermissions(session.userName, session.token));
     
-    setTimeout(function () {
-      const targetNode = document.getElementsByTagName("body")[0];
-      observer.observe(targetNode, config);
-    }, 2000);
-  });
+//     setTimeout(function () {
+//       const targetNode = document.getElementsByTagName("body")[0];
+//       observer.observe(targetNode, config);
+//     }, 2000);
+//   });
 
-  function getUserPermissions(userEmail, token) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "https://linnworks-apps.brainence.info/api/getUserConfiguration?userEmail="+userEmail, false);
-    xmlHttp.setRequestHeader('Authorization', token);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
-  }
-});
+//   function getUserPermissions(userEmail, token) {
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.open("GET", "https://linnworks-apps.brainence.info/api/getUserConfiguration?userEmail="+userEmail, false);
+//     xmlHttp.setRequestHeader('Authorization', token);
+//     xmlHttp.send(null);
+//     return xmlHttp.responseText;
+//   }
+// });
